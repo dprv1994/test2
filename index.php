5e3d1b7d2a6e9d00ec46752d11160445f0b39c8f
dprv1994 <dprv.1994@gmail.com>
2016-11-16 10:21:26 +0100

    index
    sdvbsrbdtnt,j

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Essai github
-	</title>
-</head>
-<body>
-	<p> Non Vava, c'est moi qui test
-	</p>
-</body>
-</html>
+<?php 
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Modifier index</title>
+ </head>
+ <body>
+   <h2>Daniel</h2>
+ </body>
+ </html>
